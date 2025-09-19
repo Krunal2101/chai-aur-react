2225ef0d106870bef63a0c04c0915df0a5b1295e
Krunal2101 <krunalpatel6821@gmail.com>
2025-09-19 23:01:04 +0530

    home,about,contact are ready

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,9 +2,47 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home'
+import Header from './Components/Header/Header'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './layout'
+import About from './Components/About/About'
+import Contact from './Contact/Contact'
+
+// const router = createBrowserRouter([
+//     {
+//       path:'/',
+//       element: <Layout/>,
+//       children:[
+//         {
+//           path:"",
+//           element:<Home/>
+//         },
+//         {
+//           path:'About',
+//           element: <About/>
+//         },
+//         {
+//           path:'Contact',
+//           element:<Contact/>
+//         }
+//       ]
+//     }
+//   ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+    <Route path='' element={<Home/>}/>
+    <Route path='About' element={<About/>}/>
+    <Route path='Contact' element={<Contact/>}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
