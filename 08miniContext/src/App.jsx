51e044c6eeb8232fb91bf2ab09f1e660ec43b254
Krunal2101 <krunalpatel6821@gmail.com>
2025-09-20 21:44:06 +0530

    useContext

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,15 @@
+import UserContextProvider from "./Context/userContextProvider"
+
+
+
+function App() {
+
+
+  return (
+    <UserContextProvider>
+     <h1>Mini Context API</h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
